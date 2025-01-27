c4c711f8a9776f5a2d079d7b65ecbd979cdc3075
Sofia <sofia_lobenko@epam.com>
2025-01-27 16:43:45 +0400

    Minor fix

--- services/queue_service.py
+++ services/queue_service.py
@@ -9,18 +9,14 @@ from models.image_metadata import ImageMetadata
 class QueueService:
 
     def __init__(self):
-        self.sqs = boto3.resource('sqs', AppConfig.REGION)
         self.queue_url = AppConfig.QUEUE_URL
+        self.queue = boto3.resource('sqs', AppConfig.REGION).Queue(self.queue_url)
 
     def send_image_uploaded_message(self, metadata: ImageMetadata):
-        self.sqs.send_message(
-            QueueUrl=self.queue_url,
-            MessageBody=json.dumps(metadata.to_dict())
-        )
+        self.queue.send_message(MessageBody=json.dumps(metadata.to_dict()))
 
     def get_all_messages(self):
-        queue = self.sqs.Queue(self.queue_url)
-        return queue.receive_messages(
+        return self.queue.receive_messages(
             QueueUrl=self.queue_url,
             MaxNumberOfMessages=10,
             WaitTimeSeconds=20
